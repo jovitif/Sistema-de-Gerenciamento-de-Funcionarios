f6cdf66df5f48f2e274a06efdbdb65dcced068ab
jovitif <joaosales911@gmail.com>
2024-08-17 08:31:31 -0300

    fix: adicionando metodos no controller de departamento

--- sistema_gerenciamento_funcionarios/app/controllers/departamentos_controller.rb
+++ sistema_gerenciamento_funcionarios/app/controllers/departamentos_controller.rb
@@ -1,4 +1,49 @@
 class DepartamentosController < ApplicationController
+  before_action :set_departamento, only: %i[show edit update destroy]
+
   def index
+    @departamentos = Departamento.all
+  end
+
+  def show
+  end
+
+  def new
+    @departamento = Departamento.new
+  end
+
+  def create
+    @departamento = Departamento.new(departamento_params)
+    if @departamento.save
+      redirect_to @departamento, notice: 'Departamento criado com sucesso.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @departamento.update(departamento_params)
+      redirect_to @departamento, notice: 'Departamento atualizado com sucesso.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @departamento.destroy
+    redirect_to departamentos_url, notice: 'Departamento excluído com sucesso.'
+  end
+
+  private
+
+  def set_departamento
+    @departamento = Departamento.find(params[:id])
+  end
+
+  def departamento_params
+    params.require(:departamento).permit(:nome, :descricao, :codigo, :localizacao, :status)
   end
 end
