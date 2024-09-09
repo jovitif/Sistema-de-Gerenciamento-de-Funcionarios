ed8a1ac4ea54fbd112a3135a39725df538546dc5
jovitif <joaosales911@gmail.com>
2024-09-09 18:32:31 -0300

    fix: corrigindo redefinir senha

--- sistema_gerenciamento_funcionarios/config/environments/development.rb
+++ sistema_gerenciamento_funcionarios/config/environments/development.rb
@@ -35,11 +35,22 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',  # Ou 'alunos.ufersa.edu.br', se aplicável
+    user_name:            'joaosales911@gmail.com',
+    password:             'ikxx yltg gciv qnve',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
+    config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.perform_caching = false
-
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
